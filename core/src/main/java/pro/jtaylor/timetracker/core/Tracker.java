4712363ef359abafa9e4fffc03d49f960291697a
John Riley <jriley555@yahoo.com>
2016-03-20 18:57:00 -0500

    add javadocs to lower checkstyle warnings/errors

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -11,18 +11,34 @@ public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
+    /**
+    * Add Method
+    * @Param TimeEntry time to be added
+    * /
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
+    /**
+    * Remove Method
+    * @Param TimeEntry time to be removed
+    * /
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
 
+    /**
+    * Size Method
+    * @Param Size of collection
+    * /
     public int size() {
         return entries.size();
     }
 
+    /**
+    * Get Method
+    * @Param index into collection
+    * /
     public TimeEntry get(int index) {
         return entries.get(index);
     }
